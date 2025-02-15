60f55539a8758860533a8a7795180084a2931394
Dan Sylvain <dansylvain84@gmail.com>
2025-02-15 11:23:32 +0100

    charc moves right way

--- src/display_win.c
+++ src/display_win.c
@@ -95,16 +95,16 @@ void	display_changed_elements(t_game_data	*data, int origin[2], int destination[
 		x = 0;
 		while (x < data->meta.map_size_x)
 		{
-			if (data->map[y][x].type == wall && ((x == origin[0] && y == origin[1]) || (x == destination[0] && y == destination[1])))
+			if (data->map[y][x].type == wall && ((x == destination[0] && y == destination[1]) || (x == origin[0] && y == origin[1])))
 				mlx_put_image_to_window(data->win.mlx, data->win.win,
 					data->sprites[S_ROCK_00].ptr, x * 64, y * 64);
-			if (data->map[y][x].type == collectible && ((x == origin[0] && y == origin[1]) || (x == destination[0] && y == destination[1])))
+			if (data->map[y][x].type == collectible && ((x == destination[0] && y == destination[1]) || (x == origin[0] && y == origin[1])))
 				mlx_put_image_to_window(data->win.mlx, data->win.win,
 					data->sprites[S_CRYSTAL_20].ptr, x * 64, y * 64);
-			if (data->map[y][x].is_exit_door == 1 && data->meta.coll_num == 0 && ((x == origin[0] && y == origin[1]) || (x == destination[0] && y == destination[1])))
+			if (data->map[y][x].is_exit_door == 1 && data->meta.coll_num == 0 && ((x == destination[0] && y == destination[1]) || (x == origin[0] && y == origin[1])))
 				mlx_put_image_to_window(data->win.mlx, data->win.win,
 					data->sprites[S_CRYSTAL_41].ptr, x * 64, y * 64);
-			if (data->map[y][x].type == position && ((x == origin[0] && y == origin[1]) || (x == destination[0] && y == destination[1])))
+			if (data->map[y][x].type == position && ((x == destination[0] && y == destination[1]) || (x == origin[0] && y == origin[1])))
 				mlx_put_image_to_window(data->win.mlx, data->win.win,
 					data->sprites[S_CHAR_SPRITE].ptr, x * 64, y * 64);
 			x++;
